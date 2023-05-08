bb55e0760dc04725faa660aae6e6fd90a3c5a8a6
ritakool <jokernguyen1994@gmail.com>
2023-05-08 10:33:37 +0700

    thuc hanh nhap du lieu

--- Quanlysinhvien.sql
+++ Quanlysinhvien.sql
@@ -36,3 +36,24 @@ foreign key (SubId) references Subject (SubId),
 foreign key (StudentId) references Student (StudentId)
 );
 
+INSERT INTO Class 
+VALUES 
+	(1, 'A1', '2008-12-20', 1), 
+	(2, 'A2', '2008-12-22', 1), 
+	(3, 'B3', current_date, 0);
+
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId) 
+VALUES  ('Hung', 'Ha Noi', '0912113113', 1, 1),
+		('Hoa', 'Hai phong', '09772221', 1, 1),
+		('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO Subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+       
+INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+VALUES	(1, 1, 8, 1),
+		(1, 2, 10, 2),
+        (2, 1, 12, 1);
